0d34b157cdcf5c3ccf7b886f8f695cf0302020a3
montfortkorir <ipkipkorir@gmail.com>
2024-04-09 13:36:45 +0300

    Added Assignment for week 10

--- books.sql
+++ books.sql
@@ -19,7 +19,8 @@ VALUES
     (1, 'Database Programming with MySQL', 'Fatima John', 'Computer Science', 2020),
     (2, 'Front End Development with REACT Library', 'John Doe', 'Computer Scince', 2024),
     (3, 'African History', 'George Wepughulu', 'African History', 2020),
-    (4, 'Things Fall Apart', 'Chinua Achebe', 'African Literature', 1958);
+    (4, 'Things Fall Apart', 'Chinua Achebe', 'African Literature', 1958),
+    (5, 'The Immitation of Christ', 'Thomas A` Kempis', 'Christian', 1424);
 
 -- perform data manipulation
 -- Retrieve all columns for books published in the year 2020
@@ -33,3 +34,23 @@ UPDATE books SET Genre='Computer Science' WHERE BookID=2;
 
 -- Alias the column Author as BookAuthor in a query result
 SELECT Title, Author AS BookAuthor, Genre, PublicationYear FROM books;
+
+-- modify column (PublicationYear) datatype from 'year' to 'int'
+ALTER TABLE books MODIFY COLUMN PublicationYear INT;
+
+
+-- WEEK 10 ASSIGNMENT
+-- -----------------------
+-- ORDER BY 'Title' column in ascending order and
+-- ORDER BY 'PublicationYear' column in descending order
+-- ASC (ascending order)
+-- DESC (descending order)
+ SELECT * FROM books ORDER BY Title ASC, PublicationYear DESC;
+
+-- ORDER BY 'PublicationYear' column only in descending order
+-- DESC (descending order)
+SELECT * FROM books ORDER BY PublicationYear DESC;
+
+-- ORDER BY 'Title' column only in ascending order
+-- ASC (ascending order)
+SELECT * FROM books ORDER BY Title ASC;
